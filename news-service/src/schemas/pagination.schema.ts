018a4b7144de62a8018a7b666154321f1fa85920
Jyoti Prakash Panda <147559530+jytpr5221@users.noreply.github.com>
2025-06-19 03:34:02 +0530

    updated pagination

--- news-service/src/schemas/pagination.schema.ts
+++ news-service/src/schemas/pagination.schema.ts
@@ -2,10 +2,16 @@ import z from 'zod';
 
 export const PaginationSchema = z.object({
 
-    offset: z.string().refine((val) => !isNaN(Number(val)) || Number(val)<1, {
+    offset: z.string({
+      required_error: "Offset is required",
+      invalid_type_error: "Offset must be a string"
+    }).refine((val) => !isNaN(Number(val)) || Number(val)<1, {
       message: "Offset is invalid"
     }),
-    limit: z.string().refine((val) => !isNaN(Number(val)) || Number(val)<1, {
+    limit: z.string({
+      required_error: "Limit is required",
+      invalid_type_error: "Limit must be a string"
+    }).refine((val) => !isNaN(Number(val)) || Number(val)<1, {
       message: "Limit is invalid"
     }),
   })
